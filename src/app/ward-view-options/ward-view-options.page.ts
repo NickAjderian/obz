9c73fcb55597ec6593737118e4d72d383916daa0
Nick Ajderian <nick@flikk.net>
2019-02-17 21:43:24 +0000

    getter/setter for currentWardId, bind patients

--- src/app/ward-view-options/ward-view-options.page.ts
+++ src/app/ward-view-options/ward-view-options.page.ts
@@ -7,19 +7,18 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./ward-view-options.page.scss'],
 })
 export class WardViewOptionsPage implements OnInit {
-  public currentWardId: string;
+  private _currentWardId: string;
   constructor(public data: DataService) { }
 
   ngOnInit() {
-    console.log(this.data.currentWard.ward_id);
-    this.currentWardId = this.data.currentWard.ward_id;
-
   }
-  onSelectWard() {
-    console.log('changing ward to ' + this.currentWardId);
-    this.data.currentWard = this.data.wards.filter(w => w.ward_id === this.currentWardId)[0];
-    console.log(this.data.currentWard.ward_id);
-
+  public get currentWardId (): string {
+    this._currentWardId = this.data.currentWard.ward_id;
+    return this._currentWardId;
+  }
+  public set currentWardId (value: string) {
+    this._currentWardId = value;
+    this.data.currentWard = this.data.wards.filter(w => w.ward_id === this._currentWardId)[0];
   }
 
 }
