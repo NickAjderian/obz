735e4737238c336e780cf3b45e489a5c86079827
Nick Ajderian <nick@flikk.net>
2019-02-21 09:06:48 +0000

    loading template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    'apiKey': 'AIzaSyBGYTmSg9GhOmdH6nQ-fcQLFSo8gMhprSk',
+    'databaseURL': 'https://flikkhellofirebase.firebaseio.com',
+    'databaseURL1': 'localhost:5000',
+    'storageBucket': 'flikkhellofirebase.appspot.com',
+    'authDomain': 'flikkhellofirebase.firebaseapp.com',
+    'messagingSenderId': '925963077241',
+    'projectId': 'flikkhellofirebase'
+  }
 };
